a8cc4b370e57b4f8f9013e776bcaa35b0f6a1394
mzp <mzpppp@gmail.com>
2017-03-19 22:07:07 +0900

    :wrench: setup JS compile environment

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./src/view/index.js",
+  devtool: "inline-source-map",
+  output: {
+    path: "public",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, "src/view"),
+        ],
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, "lib/js/src"),
+      "node_modules"
+    ],
+    extensions: ['.js', '.jsx']
+  }
+};
