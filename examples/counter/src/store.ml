f3a5322e40bf311b6bec10675ffcd3b043988116
mzp <mzpppp@gmail.com>
2017-03-20 19:47:47 +0900

    :gift: counter reducer

--- examples/counter/src/store.ml
+++ examples/counter/src/store.ml
@@ -1,2 +1,30 @@
-let n = Ripple.fact 10
-let () = Js.log n
+(* define action *)
+module M = Ripple.Store(struct
+  type t = [ `Inc | `Dec | `Set of int ]
+end)
+
+(* define reducer *)
+let value = M.Primitive.int 0 (fun n ->
+  function
+    | `Inc -> n + 1
+    | `Dec -> n - 1
+    | `Set m -> m)
+
+let count = M.Primitive.int 0 (fun n ->
+  function
+    | `Inc | `Dec -> n + 1
+    | `Set _ -> 0)
+
+let store =
+  let open M.Object in
+  make @@
+    ("value", value) @+
+    ("count", count) @+
+    nil
+
+(* FFI *)
+let inc = `Inc
+let dec = `Dec
+let set n = `Set n
+
+include M.Dispatch
