9d2b4150c605ae8305c9f588d2b890f77f0f0c7c
mzp <mzpppp@gmail.com>
2017-03-24 08:46:41 +0900

    :camel: add ppx_variants_conv

--- config/docker/Dockerfile.js
+++ config/docker/Dockerfile.js
@@ -4,7 +4,7 @@ WORKDIR /home/opam
 
 RUN git clone https://github.com/ocaml/opam-repository.git && \
       opam update && \
-      opam install -y merlin ocp-indent && \
+      opam install -y merlin ocp-indent utop ppx_variants_conv && \
       rm -rf /home/opam/opam-repository
 
 WORKDIR /ripple
