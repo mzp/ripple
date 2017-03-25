2aaa51fc2158b9b1a04d7e961156c14607f617fb
mzp <mzpppp@gmail.com>
2017-03-25 16:33:19 +0900

    :sparkles: add util function

--- src/ripple/ripple.ml
+++ src/ripple/ripple.ml
@@ -2,3 +2,7 @@ module Store = Ripple_store
 module Primitive = Ripple_primitive
 module Object = Ripple_object
 module Array = Ripple_array
+
+module Json = struct
+  external jsonify : 'a Js.t -> Js.Json.t = "%identity"
+end
