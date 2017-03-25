76cce954610a9bba6bafbf9f222b99af3741316a
mzp <mzpppp@gmail.com>
2017-03-25 23:10:21 +0900

    :sparkles: add lift function to convert 'a store to 'a list store

--- src/ripple/ripple_array.ml
+++ src/ripple/ripple_array.ml
@@ -1,5 +1,15 @@
+let makeArray f xs =
+  Ripple_json.array_ @@ Array.of_list @@ List.map f xs
+
 let make state jsonify reducer = {
   Ripple_store.state;
   reducer;
-  jsonify = fun xs -> Ripple_json.array_ @@ Array.of_list @@ List.map jsonify xs
+  jsonify = makeArray jsonify
+}
+
+let lift { Ripple_store.jsonify; reducer } state f = {
+  Ripple_store.state;
+  jsonify = makeArray jsonify;
+  reducer = fun xs action ->
+    List.map (fun x -> reducer x action) @@ f xs action
 }
--- src/ripple/ripple_array.mli
+++ src/ripple/ripple_array.mli
@@ -1,1 +1,3 @@
 val make : 'b list -> ('b -> Js.Json.t) -> ('b list -> 'a -> 'b list) -> ('a, 'b list) Ripple_store.t
+
+val lift : ('a, 'b) Ripple_store.t -> 'b list -> ('b list -> 'a -> 'b list) -> ('a, 'b list) Ripple_store.t
