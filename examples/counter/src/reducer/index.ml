076582eeef9ecbf9ef1bbdca543585de5e2e9207
mzp <mzpppp@gmail.com>
2017-03-25 16:26:23 +0900

    :gift: refine counter example

--- examples/counter/src/reducer/index.ml
+++ examples/counter/src/reducer/index.ml
@@ -1,33 +1,5 @@
-(* define action *)
-module Action = struct
-  type t = [
-    | `Inc
-    | `Dec
-    | `Set of int ] [@@deriving variants]
-end
-
-module M = Ripple.Store(struct
-  type t = Action.t
-end)
-
-(* define reducer *)
-let value = M.Primitive.int 0 (fun n ->
-  function
-    | `Inc -> n + 1
-    | `Dec -> n - 1
-    | `Set m -> m)
-
-let count = M.Primitive.int 0 (fun n ->
-  function
-    | `Inc | `Dec -> n + 1
-    | `Set _ -> 0)
-
-let store =
-  let open M.Object in
-  M.Store.create @@ make @@
-    ("value", value) @+
-    ("count", count) @+
-    nil
-
-(* FFI *)
+(* export all actions *)
 include Action
+
+(* export store *)
+let store = Ripple.Store.to_js (Store.store ())
