8ede098ee50b4029c8a26191ae222be3e6cd1b32
mzp <mzpppp@gmail.com>
2017-04-05 22:48:04 +0900

    :sparkles: add Ripple.Object.lift

--- src/ripple/ripple_object.mli
+++ src/ripple/ripple_object.mli
@@ -5,3 +5,5 @@ val make : ('a, 'b) t -> ('a, 'b) Ripple_reducer.t
 val nil : ('a, unit) t
 val (@+) : string * ('a, 'b) Ripple_reducer.t -> ('a, 'c) t -> ('a, 'b * 'c) t
 val (+>) : string -> ('a, 'b) Ripple_reducer.t -> string * ('a, 'b) Ripple_reducer.t
+
+val lift : ('a, 'b) Ripple_reducer.t -> (string * 'b) list -> ((string * 'b) list -> 'a -> (string * 'b) list) -> ('a, (string * 'b) list) Ripple_reducer.t
