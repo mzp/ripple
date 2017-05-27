3e4a8f40dadbd8de88f83dec0b0274e5540613dd
mzp <mzpppp@gmail.com>
2017-05-27 15:51:27 +0900

    :sparkles: collect reducer combinator like array

--- src/ripple/ripple_array.mli
+++
@@ -1,3 +0,0 @@
-val make : 'b list -> ('b -> Js.Json.t) -> ('b list -> 'a -> 'b list) -> ('a, 'b list) Ripple_reducer.t
-
-val lift : ('a, 'b) Ripple_reducer.t -> 'b list -> ('b list -> 'a -> 'b list) -> ('a, 'b list) Ripple_reducer.t
